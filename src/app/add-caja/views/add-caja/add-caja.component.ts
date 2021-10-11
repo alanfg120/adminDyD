5980113681a3f2facaf7f29c84b154a79555c4f6
Alan Fabian Herrera <alanfg120@gmail.com>
2021-10-11 10:03:01 -0500

    form addcaja pt1

--- src/app/add-caja/views/add-caja/add-caja.component.ts
+++ src/app/add-caja/views/add-caja/add-caja.component.ts
@@ -4,16 +4,37 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-add-caja',
   templateUrl: './add-caja.component.html',
-  styleUrls: ['./add-caja.component.css']
+  styleUrls: ['./add-caja.component.css'],
 })
 export class AddCajaComponent implements OnInit {
-
-  constructor() { }
+  fecha = new Date().toISOString();
+  options;
+  meses = [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre',
+  ];
+  constructor() {}
 
   ngOnInit(): void {
+    this.options = {
+      buttons: [
+        {
+          text: 'Guardar',
+          handler: () => console.log('Clicked Save!'),
+        },
+      ],
+    };
   }
 
-   addCaja(form: NgForm){
-
-   }
+  addCaja(form: NgForm) {}
 }
--- src/app/cajas/views/cajas/cajas.component.ts
+++ src/app/cajas/views/cajas/cajas.component.ts
@@ -7,38 +7,7 @@ import { CajasQuery } from '../../store/cajas.query';
   styleUrls: ['./cajas.component.css'],
 })
 export class CajasComponent implements OnInit {
-  fechaActual = new Date().toISOString();
-  options;
-  meses = [
-    'Enero',
-    'Febrero',
-    'Marzo',
-    'Abril',
-    'Mayo',
-    'Junio',
-    'Julio',
-    'Agosto',
-    'Septiembre',
-    'Octubre',
-    'Noviembre',
-    'Diciembre',
-  ];
   constructor(public store: CajasQuery) {}
 
-  ngOnInit(): void {
-    this.options = {
-      buttons: [
-        {
-          text: 'Guardar',
-          handler: () => console.log('Clicked Save!'),
-        },
-        {
-          text: 'Cancelar',
-          handler: () => {
-            console.log('Clicked Log. Do not Dismiss.');
-          },
-        },
-      ],
-    };
-  }
+  ngOnInit(): void {}
 }
